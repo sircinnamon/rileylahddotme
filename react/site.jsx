64e4219ca3e3007b402673dd786a9c0353d00bd5
Riley Lahd <sircinnamon@gmail.com>
2020-07-23 20:02:06 -0600

    Restructure repo for react dev

---
+++ react/site.jsx
@@ -0,0 +1,21 @@
+/* global React */
+/* exported Site */
+class Site extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+		}
+
+	}
+
+	render() {
+		return (
+			<div>
+				Placeholder
+			</div>
+		)
+	}
+}
+
+Site.propTypes = {
+}

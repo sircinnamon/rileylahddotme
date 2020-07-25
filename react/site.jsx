6d1388e056ccca0b3d479e90f05401919a482a5d
Riley Lahd <sircinnamon@gmail.com>
2020-07-24 20:51:15 -0600

    Add functional headerbuttons and terminalWindow class

--- react/site.jsx
+++ react/site.jsx
@@ -18,7 +18,10 @@ class Site extends React.Component {
 			let newWindow = {
 				pos: { x: Math.random() * 200, y: Math.random() * 200 },
 				id: id,
-				title: id
+				title: counter % 2 == 0 ? "Terminal "+id:id,
+				folded: false,
+				hidden: false,
+				type: counter % 2 == 0 ? "terminal" : "default"
 			};
 			newWindow.children = (
 				<div
@@ -31,7 +34,7 @@ class Site extends React.Component {
 				>
 					Test
 				</div>
-			)
+			);
 			winSet[id] = newWindow;
 			this.setState({ windows: winSet, windowCounter: counter + 1 });
 		};
@@ -91,6 +94,33 @@ class Site extends React.Component {
 				windowZOrder: zOrder
 			});
 		};
+
+		this.toggleFoldWindow = function (ev, id) {
+			ev.stopPropagation();
+			let winSet = this.state.windows;
+			winSet[id].folded = !winSet[id].folded;
+			this.setState({
+				windows: winSet
+			});
+		};
+
+		this.hideWindow = function (ev, id) {
+			ev.stopPropagation();
+			let winSet = this.state.windows;
+			winSet[id].hidden = true;
+			this.setState({
+				windows: winSet
+			});
+		};
+
+		this.showWindow = function (ev, id) {
+			ev.stopPropagation();
+			let winSet = this.state.windows;
+			winSet[id].hidden = false;
+			this.setState({
+				windows: winSet
+			});
+		};
 	}
 
 	componentDidMount() {
@@ -106,26 +136,63 @@ class Site extends React.Component {
 
 	render() {
 		let windows = Object.entries(this.state.windows).map((w) => {
-			return (
-				<Window
-					title={w[1].title}
-					topPos={w[1].pos.y}
-					leftPos={w[1].pos.x}
-					grabWindow={(ev) => {
-						this.grabWindow(ev, w[1].id);
-					}}
-					isHeld={w[1].id === this.state.heldWindow}
-					makeActive={(ev) => {
-						this.makeWindowActive(ev, w[1].id);
-					}}
-					layer={this.state.windowZOrder.indexOf(w[1].id)}
-					close={(ev) => {
-						this.deleteWindow(ev, w[1].id);
-					}}
-				>
-					{w[1].children}
-				</Window>
-			);
+			let globalProps = {
+				title: w[1].title,
+				topPos: w[1].pos.y,
+				leftPos: w[1].pos.x,
+				grabWindow: (ev) => {
+					this.grabWindow(ev, w[1].id);
+				},
+				isHeld: w[1].id === this.state.heldWindow,
+				isHidden: w[1].hidden,
+				isFolded: w[1].folded,
+				makeActive: (ev) => {
+					this.makeWindowActive(ev, w[1].id);
+				},
+				layer: this.state.windowZOrder.indexOf(w[1].id),
+				close: (ev) => {
+					this.deleteWindow(ev, w[1].id);
+				},
+				toggleFold: (ev) => {
+					this.toggleFoldWindow(ev, w[1].id);
+				},
+				hide: (ev) => {
+					this.hideWindow(ev, w[1].id);
+				}
+			};
+			if (w[1].type === "terminal") {
+				let bodyChunks = [
+					{ string: "pi@thor", bold: true, color: 0 },
+					{ string: ":" },
+					{ string: "~/rileylahddotme ", bold: true, color: 1 },
+					{ string: "python3 -m http.server\n" },
+					{
+						string: "Serving HTTP on 0.0.0.0 port 8000 (http://0.0.0.0:8000/) ...\n"
+					},
+					{
+						string: '10.0.0.117 - - [24/Jul/2020 15:12:40] "GET / HTTP/1.1" 200 -\n'
+					},
+					{
+						string:
+							'10.0.0.117 - - [24/Jul/2020 15:12:40] "GET /js/main.js HTTP/1.1" 200 -\n'
+					},
+					{
+						string:
+							'10.0.0.117 - - [24/Jul/2020 15:12:40] "GET /js/rileylahddotme-react.min.js HTTP/1.1" 200 -\n'
+					},
+					{
+						string:
+							"10.0.0.117 - - [24/Jul/2020 15:12:40] code 404, message File not found\n"
+					},
+					{
+						string:
+							'10.0.0.117 - - [24/Jul/2020 15:12:40] "GET /favicon.ico HTTP/1.1" 404 -\n'
+					}
+				];
+				return <TerminalWindow bodyChunks={bodyChunks} {...globalProps} />;
+			} else {
+				return <Window {...globalProps}>{w[1].children}</Window>;
+			}
 		});
 		return (
 			<div>
--- react/window.jsx
+++ react/window.jsx
@@ -6,24 +6,62 @@ class Window extends React.Component {
 
 	render() {
 		let windowStyle = {
-			display: "inline-block",
+			display: this.props.isHidden ? "none" : "inline-block",
 			minWidth: "40px",
 			minHeight: "20px",
 			position: "absolute",
 			top: Math.max(this.props.topPos, 0) || 0,
 			left: Math.max(this.props.leftPos, 0) || 0,
-			zIndex: this.props.layer + 100,
+			zIndex: this.props.layer + 100
 		};
 		let headerStyle = {
-			width: "100%",
 			backgroundColor: "rgba(0,0,0,0.9)",
 			userSelect: "none",
-			color: "rgb(75,75,75)"
+			color: "rgb(75,75,75)",
+			padding: "4px 4px 0px",
+			borderRadius: "5px 5px 0px 0px"
 		};
 		if (this.props.isHeld) {
 			headerStyle.backgroundColor = "rgba(255,0,0,0.7)";
 		}
 		let closeButtonStyle = {
+			background: "rgba(200, 30, 30, 0.5)"
+		};
+		let foldButtonStyle = {
+			background: "rgba(150, 150, 30, 0.5)"
+		};
+		let hideButtonStyle = {
+			background: "rgba(30, 200, 30, 0.5)"
+		};
+		let bodyStyle = {
+			maxHeight: this.props.isFolded ? "0px" : "",
+			overflow: this.props.isFolded ? "hidden" : ""
+		};
+		return (
+			<div style={windowStyle} onMouseDown={this.props.makeActive}>
+				<div style={headerStyle} onMouseDown={this.props.grabWindow}>
+					{this.props.title}
+					<WindowHeaderBtn style={closeButtonStyle} onMouseDown={this.props.close} />
+					<WindowHeaderBtn
+						style={foldButtonStyle}
+						onMouseDown={this.props.toggleFold}
+					/>
+					<WindowHeaderBtn style={hideButtonStyle} onMouseDown={this.props.hide} />
+				</div>
+				<div style={bodyStyle}>{this.props.children}</div>
+			</div>
+		);
+	}
+}
+
+class WindowHeaderBtn extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		let style = {
 			height: "10px",
 			width: "10px",
 			float: "right",
@@ -31,16 +69,55 @@ class Window extends React.Component {
 			fontSize: "5px",
 			margin: "2px",
 			borderRadius: "10px",
-			background: "rgba(200, 30, 30, 0.5)"
+			background: "rgba(200, 30, 30, 0.7)",
+			...this.props.style
+		};
+		return <div style={style} onMouseDown={this.props.onMouseDown} />;
+	}
+}
+
+class TerminalWindow extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		let colors = ["#20872b", "#205c87"];
+		let body = [];
+		for (let i = 0; i < this.props.bodyChunks.length; i++) {
+			let s = {};
+			let c = this.props.bodyChunks[i];
+			if (c.color !== undefined) {
+				s.color = colors[c.color];
+			}
+			if (c.bold) {
+				s.fontWeight = "bold";
+			}
+			body.push(<span style={s}>{c.string}</span>);
+		}
+		let preStyle = {
+			backgroundColor: "rgba(0,0,0,0.7)",
+			color: "rgb(200,200,200)",
+			margin: 0
 		};
 		return (
-			<div style={windowStyle} onMouseDown={this.props.makeActive}>
-				<div style={headerStyle} onMouseDown={this.props.grabWindow}>
-					{this.props.title}
-					<div style={closeButtonStyle} onMouseDown={this.props.close} />
-				</div>
-				<div>{this.props.children}</div>
-			</div>
+			<Window
+				title={this.props.title}
+				topPos={this.props.topPos}
+				leftPos={this.props.leftPos}
+				grabWindow={this.props.grabWindow}
+				isHeld={this.props.isHeld}
+				isHidden={this.props.isHidden}
+				isFolded={this.props.isFolded}
+				makeActive={this.props.makeActive}
+				layer={this.props.layer}
+				close={this.props.close}
+				toggleFold={this.props.toggleFold}
+				hide={this.props.hide}
+			>
+				<pre style={preStyle}>{body}</pre>
+			</Window>
 		);
 	}
 }

e673ea849db8181b975d37b4b470adc940cf6c72
Riley Lahd <sircinnamon@gmail.com>
2024-03-04 13:51:23 -0700

    Lint fixes

--- modules/HomeView.jsx
+++ modules/HomeView.jsx
@@ -1,5 +1,4 @@
-import React from "react"
-import {useState} from "react"
+import React, { useState } from "react"
 import profile from "../public/img/profile.jpg"
 import cinder from "../public/img/cinder.jpg"
 import github from "../public/img/github.svg"
@@ -7,10 +6,9 @@ import linkedin from "../public/img/linkedin.png"
 import envelope from "../public/img/mail.png"
 
 const HomeView = () => {
-
 	const [imgSrc, setImgSrc] = useState(profile)
-	const toggleImgSrc = ()=>{
-		setImgSrc((imgSrc===profile)?cinder:profile)
+	const toggleImgSrc = () => {
+		setImgSrc((imgSrc === profile) ? cinder : profile)
 	}
 
 	return (
@@ -42,22 +40,22 @@ const HomeView = () => {
 				</div>
 				<div>
 					<h1 id="main_title">RILEY LAHD</h1>
-					<p className="subtitle" style={{color: "#9d65ff"}}> &gt; Full-Stack Developer</p>
+					<p className="subtitle" style={{ color: "#9d65ff" }}> &gt; Full-Stack Developer</p>
 				</div>
 			</div>
 			<div>
 				<h2 className="section-header">ABOUT</h2>
-				<p className="blurb" style={{borderColor: "#58d1eb"}}>
-					Hello! I'm Riley, a Software Developer from Calgary, Alberta. I'm passionate about coding but my other hobbies include gaming (tabletop, video, and board games), scuba diving, movies, and more. I consider myself a fast-learner, detail oriented, and able to communicate and collaborate effectively with project managers and stakeholders.
+				<p className="blurb" style={{ borderColor: "#58d1eb" }}>
+					{"Hello! I'm Riley, a Software Developer from Calgary, Alberta. I'm passionate about coding but my other hobbies include gaming (tabletop, video, and board games), scuba diving, movies, and more. I consider myself a fast-learner, detail oriented, and able to communicate and collaborate effectively with project managers and stakeholders."}
 					<br/>
 					<br/>
-					I also have a cat named <span onClick={toggleImgSrc} style={{cursor:"pointer"}}>Cinder!</span>
+					I also have a cat named <span onClick={toggleImgSrc} style={{ cursor: "pointer" }}>Cinder!</span>
 				</p>
 
 			</div>
 			<div>
 				<h2 className="section-header">EDUCATION</h2>
-				<p className="blurb" style={{borderColor: "#98e024"}}>
+				<p className="blurb" style={{ borderColor: "#98e024" }}>
 					<b>Bachelor of Science in Computer Science - University of Calgary</b>
 					<br/>
 					<i>2012-2017</i>
@@ -75,7 +73,7 @@ const HomeView = () => {
 			</div>
 			<div>
 				<h2 className="section-header">EXPERIENCE</h2>
-				<p className="blurb" style={{borderColor: "#fd971f"}}>
+				<p className="blurb" style={{ borderColor: "#fd971f" }}>
 					<b>Shaw Communications / Rogers Communications</b>
 					<br/>
 					<i>Senior Software Developer [Jul 2019 - Feb 2024, 2 yrs 8 mos]</i>
@@ -104,7 +102,7 @@ const HomeView = () => {
 			</div>
 			<div>
 				<h2 className="section-header">PORTFOLIO</h2>
-				<p className="blurb" style={{borderColor: "#f4005f"}}>
+				<p className="blurb" style={{ borderColor: "#f4005f" }}>
 					Many of my personal projects are available on my <a rel="noopener noreferrer" target="_blank" href="https://github.com/sircinnamon">GitHub</a>. Some of these highlights may be relevant:
 					<br/>
 					<br/>
@@ -115,19 +113,19 @@ const HomeView = () => {
 					<br/>
 					<a rel="noopener noreferrer" target="_blank" href="https://github.com/sircinnamon/runescape-text">Runescape Text</a>
 					<br/>
-					This project caused me to notice a bug in the popular Python image library "Pillow", which I was able to open a pull request for and fix, earning me the <b>Mars 2020 Contributor</b> badge as the library went on to be used in NASA's 2020 Marse Helicopter mission. This python script simply generates GIFs of moving text in the style of a specific video game.
+					{"This project caused me to notice a bug in the popular Python image library \"Pillow\", which I was able to open a pull request for and fix, earning me the <b>Mars 2020 Contributor</b> badge as the library went on to be used in NASA's 2020 Marse Helicopter mission. This python script simply generates GIFs of moving text in the style of a specific video game."}
 					<br/>
 					<br/>
 					<a rel="noopener noreferrer" target="_blank" href="https://sircinnamon.ca/ll/">Love Letter</a>
 					<br/>
-					During COVID it became harder to get together for board games with friends, so I took one of my group's favourites and created a browser-based version entirely in React and NodeJS!
+					{"During COVID it became harder to get together for board games with friends, so I took one of my group's favourites and created a browser-based version entirely in React and NodeJS!"}
 				</p>
 			</div>
 			<div
 				style={{
-					borderTop:"1px solid #FFFFFF",
+					borderTop: "1px solid #FFFFFF",
 					marginTop: "2em",
-					padding: "1em 5em",
+					padding: "1em 5em"
 				}}
 			>
 				<a className="footer-link" rel="noopener noreferrer" target="_blank" href="https://github.com/sircinnamon"><img className="footer-icon" src={github}/></a>
